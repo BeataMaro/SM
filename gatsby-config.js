16f05190a564e1b4c7a742123c9764d4ec143615
BeataMaro <beatamaro@interia.pl>
2021-07-14 23:29:31 +0200

    gatsby-plugin-env-variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-env-variables`,
+      options: {
+        allowList: ["MY_ENV"],
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import config from "gatsby-plugin-config"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -10,7 +9,8 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people {process.env.GATSBY_API_KEY}</h1>
-    <h2>Hi people {config.MY_ENV}</h2>
+    {/* env variable without GATSBY_ prefix - nie działa */}
+    <h2>Hi people {process.env.MY_ENV}</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
