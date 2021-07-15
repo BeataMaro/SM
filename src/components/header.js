6a2bf9d12204778d5de6a56c116710e7b17f5a39
BeataMaro <beatamaro@interia.pl>
2021-07-15 23:55:47 +0200

    gsap animations

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,45 @@
-import * as React from "react"
+import React, { useEffect, useRef } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { animationMoveY } from "../styles/Animation"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const sloganRef = useRef(null)
+
+  useEffect(() => {
+    const slogan = sloganRef.current
+    animationMoveY(slogan, true)
+  }, [])
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }} ref={sloganRef}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
