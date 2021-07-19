b6d7cdfc97294a239aab98de3499e76f2c3cbdbf
BeataMaro <beatamaro@interia.pl>
2021-07-19 13:00:43 +0200

    Contact form response

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -10,7 +10,29 @@ const StyledForm = styled.form`
 const ContactForm = () => {
   const [state, handleSubmit] = useForm("ContactSelfMAX")
   if (state.succeeded) {
-    return <p>Wiadomość wysłana pomyślnie. Dziękujemy!</p>
+    return (
+      <StyledForm onSubmit={handleSubmit}>
+        <label htmlFor="name">Imię</label>
+        <input id="name" type="text" name="name" />
+        <label htmlFor="email">Adres e-mail</label>
+        <input id="email" type="email" name="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <label htmlFor="phone">Numer telefonu</label>
+        <input id="phone" type="numeric" name="phone" />
+        <ValidationError prefix="Phone" field="phone" errors={state.errors} />
+        <label htmlFor="message">Wiadomość</label>
+        <textarea id="message" name="message" placeholder="Proszę o kontakt." />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <p>Wiadomość wysłana pomyślnie. Dziękujemy!</p>
+        <Button type="submit" disabled={state.submitting}>
+          Wyślij
+        </Button>
+      </StyledForm>
+    )
   }
   return (
     <StyledForm onSubmit={handleSubmit}>
